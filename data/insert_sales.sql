a99001a913800721b5617bff10abf8cadcad2a82
Juan Estrella <juan.estrella@finos.org>
2023-05-09 12:30:59 +0200

    add optic store data imports, model, and queries

---
+++ data/insert_sales.sql
@@ -0,0 +1,15 @@
+USE optical_store;
+insert into SALES (glasses, buyer, sales_person, sale_date)
+values ('2', '1', '1', '2023-01-02');
+insert into SALES (glasses, buyer, sales_person, sale_date)
+values ('2', '1', '3', '2023-03-10');
+insert into SALES (glasses, buyer, sales_person, sale_date)
+values ('4', '2', '2', '2023-01-25');
+insert into SALES (glasses, buyer, sales_person, sale_date)
+values ('4', '3', '3', '2023-05-01');
+insert into SALES (glasses, buyer, sales_person, sale_date)
+values ('3', '3', '1', '2023-04-22');
+insert into SALES (glasses, buyer, sales_person, sale_date)
+values ('3', '2', '2', '2023-04-10');
+insert into SALES (glasses, buyer, sales_person, sale_date)
+values ('2', '2', '3', '2023-04-14');
