05ff8e61c98f98769dccef72e4d82a0338fa9443
Juan Estrella <juan.estrella@finos.org>
2023-05-10 11:20:20 +0200

    pizzeria model

---
+++ data/pizzeria/insert_order_type.sql
@@ -0,0 +1,5 @@
+use pizzeria;
+insert into order_type (order_type)
+values ('pickup');
+insert into order_type (order_type)
+values ('delivery');
---
+++ data/pizzeria/insert_pizza_types.sql
@@ -0,0 +1,9 @@
+use pizzeria;
+insert into category (name)
+values ('meat');
+insert into category (name)
+values ('vegeterian');
+insert into category (name)
+values ('cheese');
+insert into category (name)
+values ('tomato');
